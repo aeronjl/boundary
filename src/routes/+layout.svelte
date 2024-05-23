b63b51cf534ab3ac001b28aad07c85ce1d7c759a
Aeron <ajlaffere@gmail.com>
2024-05-23 22:14:19 +0100

    some aesthetics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,33 +27,35 @@
 	}
 </script>
 
-<div class="mx-auto max-w-[600px] px-4">
+<div class="mx-auto max-w-[800px] px-4 relative">
 	{#if $page.url.pathname === '/'}
-	<div transition:fade={{duration: 200}} class="fixed top-6">
-	<div class="w-[600px] mx-auto flex flex-row items-center justify-between text-sm">
-		<svg class="h-4 w-4 text-black" viewBox="0 0 100 100">
-			<a href="/">
-				<circle cx="50" cy="50" r="40" fill="currentColor" />
-			</a>
-		</svg>
-		<ul class="flew-row flex gap-x-2 underline">
-			<li><a href="/">home</a></li>
-			<li><a href="/about">about</a></li>
-			<li><a href="https://buy.stripe.com/7sI17G4dQc1Q6yY7ss" target="_blank">donate</a></li>
-		</ul>
-	</div>
-</div>
+		<div transition:fade={{ duration: 200 }} class="absolute top-0 inset-x-0 right-4 left-4 flex flex-row items-center justify-between text-sm">
+				<svg class="h-4 w-4 text-black" viewBox="0 0 100 100">
+					<a href="/">
+						<circle cx="50" cy="50" r="40" fill="currentColor" />
+					</a>
+				</svg>
+				<ul class="flew-row flex gap-x-2 underline">
+					<li><a href="/">home</a></li>
+					<li><a href="/about">about</a></li>
+					<li><a href="https://buy.stripe.com/7sI17G4dQc1Q6yY7ss" target="_blank">donate</a></li>
+				</ul>
+		</div>
 	{/if}
 	<div class="mt-24">
 		<div class="my-4 h-[50px]">
 			{#if $page.url.pathname != '/'}
-			<div class="flex flex-row items-center gap-2 font-mono text-xs" in:fly={{ x: -10, duration: 200 }} out:fly={{ x: -10, duration: 200 }}>
-				<button on:click={goBack} class="rounded-sm bg-gray-100 px-2 py-1 h-[24px]">
-					&#x2190; Go back
-				</button>
-				<p class="text-gray-400">›</p>
-				<button class="rounded-sm py-1 truncate h-[24px] w-[120px]">{currentPage}</button>
-			</div>
+				<div
+					class="flex flex-row items-center gap-2 font-mono text-xs"
+					in:fly={{ x: -10, duration: 200 }}
+					out:fly={{ x: -10, duration: 200 }}
+				>
+					<button on:click={goBack} class="h-[24px] rounded-sm bg-gray-100 px-2 py-1">
+						&#x2190; Go back
+					</button>
+					<p class="text-gray-400">›</p>
+					<button class="h-[24px] max-w-[200px] truncate rounded-sm py-1">{currentPage}</button>
+				</div>
 			{/if}
 		</div>
 	</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,12 @@
 </script>
 
 <h1 class="mb-4 font-serif text-3xl leading-10">Boundary</h1>
-<p class="text-sm my-2">Everyone has a stake in the science of psychology.</p>
-<p class="text-sm my-2">The experiments made available on this website are transparent. The aim is to make clear how psychologists get from tests to inferences about mental qualities, cognitive abilities, and diagnoses.</p>
+<p class="my-2 text-sm">Everyone has a stake in the science of psychology.</p>
+<p class="my-2 text-sm">
+	The experiments made available on this website are transparent. The aim is to make clear how
+	psychologists get from tests to inferences about mental qualities, cognitive abilities, and
+	diagnoses.
+</p>
 
 <div class="my-4 flex flex-col gap-y-1">
 	<h3 class="font-serif text-lg">Recent Experiments</h3>
@@ -25,18 +29,4 @@
 			<a href="/ten-item-personality-inventory" class="underline">Ten Item Personality Inventory</a>
 		</li>
 	</ul>
-</div>
-
-<div class="my-4 flex hidden flex-col gap-y-1">
-	<h3 class="font-serif text-lg">Research Areas</h3>
-	<div class="grid grid-cols-3 gap-4 text-sm">
-		<ul class="list-inside list-disc">
-			<li><a href="/perception" class="underline">Perception</a></li>
-			<li>Decision Making</li>
-			<li>Attention</li>
-			<li>Memory</li>
-			<li>Personality</li>
-			<li>Psychopathology</li>
-		</ul>
-	</div>
 </div>

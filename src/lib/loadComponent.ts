cc402a0b542e5e19f244f07ca391da84e1987bd3
Aeron <ajlaffere@gmail.com>
2024-05-24 15:31:55 +0100

    types

--- src/lib/loadComponent.ts
+++ src/lib/loadComponent.ts
@@ -1,37 +1,6 @@
-/** export async function loadComponent(path: string) {
-	try {
-		switch (path) {
-			case '/orientation-discrimination':
-				return (await import('./components/orientation-discrimination/LiteratureReview.svelte'))
-					.default;
-            case '/ten-item-personality-inventory':
-                return (await import('./components/ten-item-personality-inventory/LiteratureReview.svelte'))
-                    .default;
-			default:
-				throw new Error('Component not found');
-		}
-	} catch (error) {
-		console.error(`Failed to load component for path ${path}:`, error);
-		return null;
-	}
-}  
-
-export async function loadComponent(path: string) {
-	try {
-		// Construct the dynamic import path
-		const componentPath = `./components${path}/LiteratureReview.svelte`;
-
-		// Use a dynamic import with a template literal
-		return (await import( componentPath)).default;
-	} catch (error) {
-		console.error(`Failed to load component for path ${path}:`, error);
-		return null;
-	}
-}
-
-*/
-
 // src/lib/loadComponent.ts
+import type { SvelteComponent } from 'svelte';
+
 const componentMap = import.meta.glob('/src/lib/components/**/LiteratureReview.svelte');
 const resultsMap = import.meta.glob('/src/lib/components/**/Results.svelte');
 
@@ -41,9 +10,13 @@ export async function loadComponents(path: string) {
 		const resultsPath = `/src/lib/components${path}/Results.svelte`;
 
 		const LiteratureReview = componentMap[literatureReviewPath]
-			? (await componentMap[literatureReviewPath]()).default
+			? ((await (componentMap[literatureReviewPath]() as Promise<{ default: unknown }>))
+					.default as SvelteComponent)
 			: null;
-		const Results = resultsMap[resultsPath] ? (await resultsMap[resultsPath]()).default : null;
+		const Results = resultsMap[resultsPath]
+			? ((await (resultsMap[resultsPath]() as Promise<{ default: unknown }>))
+					.default as SvelteComponent)
+			: null;
 
 		if (!LiteratureReview || !Results) {
 			throw new Error('Component not found');
